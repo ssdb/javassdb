c5494588df75d1eb7d47cc7104a518ef57d0ad90
ideawu <ideawu@me>
2015-12-07 19:41:51 +0800

    fix Response.buildMap() bug

--- com/udpwork/ssdb/Response.java
+++ com/udpwork/ssdb/Response.java
@@ -41,7 +41,7 @@ public class Response{
 	}
 	
 	public void buildMap(){
-		for(int i=1; i<raw.size()+1; i+=2){
+		for(int i=1; i+1<raw.size(); i+=2){
 			byte[] k = raw.get(i);
 			byte[] v = raw.get(i+1);
 			keys.add(k);
